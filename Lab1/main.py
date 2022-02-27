3f1dc0955335f72aae41054b5207d2e030468cb6
XoTaBuCh <tanzanit2003@gmail.com>
2022-02-27 15:23:52 -0800

    Lab1 added

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,6 @@
 import re
+import statistics
+from collections import defaultdict
 
 
 def input_file(filename: str) -> str:
@@ -6,20 +8,75 @@ def input_file(filename: str) -> str:
     return text
 
 
-def text_analytics(text: str):
-    text.lower()
-    pass
+def get_words(text: str) -> list[str]:
+    words = re.split(r"(?:(?:[^a-zA-Z]+')|(?:'[^a-zA-Z]+))|(?:[^a-zA-Z']+)", text)
+    words_tmp = []
+    for word in words:
+        if len(word) > 0:
+            words_tmp.append(word)
+    words = words_tmp
+    return words
+
+
+def get_sentences_words(text: str) -> tuple[float, float]:
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+    word_length: list[int] = []
+    for sentence in sentences:
+        if len(sentence) > 0:
+            word_length.append(len(get_words(sentence)))
+    average_amount = statistics.fmean(word_length)
+    median_amount = statistics.median(word_length)
+    return average_amount, median_amount
+
+
+def get_words_count(words: list[str]) -> dict[str, int]:
+    words_dict: dict[str, int] = defaultdict(int)
+    for word in words:
+        words_dict[word] += 1
+    return words_dict
+
+
+def get_ngrams(words_dict: dict[str, int], n: int) -> dict[str, int]:
+    ngrams: dict[str, int] = defaultdict(int)
+    for key, item in words_dict.items():
+        for window in range(0, len(key) - n + 1):
+            ngrams[key[window: window + n]] += item
+    return ngrams
+
+
+def input_data() -> tuple[int, int]:
+    n: int = input("Input N: ")
+    if n == "":
+        n = 4
+    k: int = input("Input K: ")
+    if k == "":
+        k = 10
+    return n, k
+
+
+def output_data(words_dict: dict[str, int], average_amount: float, median_amount: float,
+                sorted_ngrams: list[str], ngrams: dict[str, int], k: int):
+    print("Words dictionary:")
+    for word, amount in words_dict.items():
+        print(f"{word} = {amount}")
+    print(f"Average amount of words in sentences: {average_amount}")
+    print(f"Median amount of words in sentences: {median_amount}")
+    print("Top k  ngrams:")
+    for index in range(len(sorted_ngrams) - 1, len(sorted_ngrams) - k - 1, -1):
+        print(f" {sorted_ngrams[index][0]} = {sorted_ngrams[index][1]}")
 
 
 def main():
-    n = input("Input N: ")
-    if n == "":
-        n = 4
-    k = input("Input K: ")
-    if k == "":
-        k = 10
     text = input_file("input.txt")
-    print("Text:\n", text, sep = '')
+    n, k = input_data()
+    print("Text:\n", text, sep='')
+    text = text.lower()
+    words = get_words(text)
+    words_dict = get_words_count(words)
+    average_amount, median_amount = get_sentences_words(text)
+    ngrams = get_ngrams(words_dict, n)
+    sorted_ngrams = sorted(ngrams.items(), key=lambda x: x[1])
+    output_data(words_dict, average_amount, median_amount, sorted_ngrams, ngrams, k)
 
 
 if __name__ == "__main__":
